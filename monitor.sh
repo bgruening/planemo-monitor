d602ec94a2f3afa787c9027e7c2fad0aa3215bd4
Matthias Bernt <m.bernt@ufz.de>
2023-07-26 17:52:34 +0200

    run planemo separately for each tool
    and respect deprecated, .tt_skip, ...

--- monitor.sh
+++ monitor.sh
@@ -4,9 +4,15 @@ planemo conda_init
 
 sort -R $1 | while read repository
 do
-   repo_dir=$(basename "$repository")
-   git clone --depth=1 "$repository" "$repo_dir"
-   planemo container_register --force_push --recursive "$repo_dir"
-   rm -rf "$repo_dir"
+    repo_dir=$(basename "$repository")
+    git clone --depth=1 "$repository" "$repo_dir"
+    cd "$repo_dir"
+    planemo ci_find_repos  --exclude packages --exclude deprecated --exclude_from .tt_skip --output repository_list.txt
+    cat repository_list.txt | while read tool_dir
+    do
+        planemo container_register --force_push --recursive "$tool_dir"
+    done
+    cd -
+    rm -rf "$repo_dir"
 done
 
