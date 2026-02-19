51a08e52eea2bd166dc48267ddd9dc78507010a4
Matthias Bernt <m.bernt@ufz.de>
2026-02-19 18:52:16 +0100

    Revert "try to record exit code"
    This reverts commit 80736ea25fb98c1769d3565239cc64769e009500.

--- monitor.sh
+++ monitor.sh
@@ -2,20 +2,17 @@
 
 planemo conda_init
 
-exit_code="0"
-sort -R "$1" | while read repository
+sort -R $1 | while read repository
 do
     repo_dir=$(basename "$repository")
     git clone --depth=1 "$repository" "$repo_dir"
     cd "$repo_dir"
     planemo ci_find_repos  --exclude packages --exclude deprecated --exclude_from .tt_skip --output repository_list.txt
-    while read tool_dir
+    cat repository_list.txt | while read tool_dir
     do
         planemo container_register --force_push --recursive "$tool_dir"
-        exit_code=$(( "$exit_code" | "$?" ))
-    done < repository_list.txt
+    done
     cd -
     rm -rf "$repo_dir"
 done
 
-exit $exit_code
